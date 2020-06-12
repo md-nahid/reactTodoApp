404f55a618e6e9306535c75c8c7b8be4fe2ccaa1
md-nahid <nahid98learn@gmail.com>
2020-06-12 16:38:52 +0600

    Updates

---
+++ precache-manifest.8bf4152ce68470b7a85dee8fad847109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9753666c97a179a563cfe8f0591ffd89",
+    "url": "/reactTodoApp/index.html"
+  },
+  {
+    "revision": "e780bfcf464c97a50478",
+    "url": "/reactTodoApp/static/css/main.fa85c141.chunk.css"
+  },
+  {
+    "revision": "cfcfe7f232f240ab36b9",
+    "url": "/reactTodoApp/static/js/2.f2a900db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTodoApp/static/js/2.f2a900db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e780bfcf464c97a50478",
+    "url": "/reactTodoApp/static/js/main.214f85f0.chunk.js"
+  },
+  {
+    "revision": "7bc27145de8d1df00920",
+    "url": "/reactTodoApp/static/js/runtime-main.efa4238d.js"
+  }
+]);
